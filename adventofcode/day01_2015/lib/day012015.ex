5906319f38385eac89c9adb0cba4db399f3366fd
Adolfo Neto <adolfo.usp@gmail.com>
2021-11-29 15:03:03 -0300

    Adevnt of Code 2015 - Day 1 - Part 2

--- adventofcode/day01_2015/lib/day012015.ex
+++ adventofcode/day01_2015/lib/day012015.ex
@@ -29,10 +29,21 @@ defmodule Day012015 do
       -3
       iex> Day012015.find_floor(")())())")
       -3
-      iex>  File.read("lib/my_input.txt") |> Kernel.then(fn {:ok, string} -> String.trim(string) end) |> Day012015.find_floor()
+      iex>  Day012015.find_floor(Day012015.convert_file_to_string("lib/my_input.txt"))
       138
+      iex> Day012015.find_position("())))")
+      3
+      iex> Day012015.find_position(Day012015.convert_file_to_string("lib/my_input.txt"))
+      1771
 
   """
+  def convert_file_to_string(filename) do
+    # File.read(filename) |>
+    # Kernel.then(fn {:ok, string} -> String.trim(string) end)
+    {:ok, string} = File.read(filename)
+    string
+  end
+
   def find_floor(string) do
     find_floor(string, 0)
   end
@@ -45,5 +56,19 @@ defmodule Day012015 do
 
   def find_floor(")" <> rest, current) do
     find_floor(rest, current - 1)
+  end
+
+  def find_position(string) do
+    find_position(string, 0, 0)
+  end
+
+  def find_position(_, position, -1), do: position
+
+  def find_position("(" <> rest, position, floor) do
+    find_position(rest, position + 1, floor + 1)
+  end
+
+  def find_position(")" <> rest, position, floor) do
+    find_position(rest, position + 1, floor - 1)
   end
 end
