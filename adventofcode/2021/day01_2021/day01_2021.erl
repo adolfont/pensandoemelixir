3bebe6d82da1068d3de580b310a2960bd73c5ae0
Adolfo Neto <adolfo.usp@gmail.com>
2021-12-01 09:24:38 -0300

    Day 1 - Task 2 - Advent of Code 2021 in Erlang

--- adventofcode/2021/day01_2021/day01_2021.erl
+++ adventofcode/2021/day01_2021/day01_2021.erl
@@ -1,7 +1,9 @@
 -module(day01_2021).
 
--export([depth_measurement_increases/1, main/0]).
+-export([depth_measurement_increases/1,
+         depth_measurement_increases_from_sliding_windows/1, main/0]).
 
+%% TASK 1
 depth_measurement_increases(InputString) ->
     ListOfNumbers = convert_long_string_to_numbers(InputString),
     depth_measurement_increases(ListOfNumbers, 0).
@@ -10,12 +12,41 @@ depth_measurement_increases([First, Second], Increases) when Second > First ->
     Increases + 1;
 depth_measurement_increases([_First, _Second], Increases) ->
     Increases;
-depth_measurement_increases([First, Second | Tail], Increases) 
-    when Second > First ->
+depth_measurement_increases([First, Second | Tail], Increases) when Second > First ->
     depth_measurement_increases([Second | Tail], Increases + 1);
 depth_measurement_increases([_First, Second | Tail], Increases) ->
     depth_measurement_increases([Second | Tail], Increases).
 
+%% TASK 2
+
+depth_measurement_increases_from_sliding_windows(InputString) ->
+    ListOfNumbers = convert_long_string_to_numbers(InputString),
+    depth_measurement_increases_from_sliding_windows(ListOfNumbers, 0, 0).
+
+depth_measurement_increases_from_sliding_windows([Element1, Element2, Element3],
+                                                 PreviousWindow,
+                                                 Increases)
+    when Element1 + Element2 + Element3 > PreviousWindow ->
+    Increases + 1;
+depth_measurement_increases_from_sliding_windows([_Element1, _Element2, _Element3],
+                                                 _PreviousWindow,
+                                                 Increases) ->
+    Increases;
+depth_measurement_increases_from_sliding_windows([E1, E2, E3, E4 | Rest],
+                                                 _PreviousWindow,
+                                                 Increases)
+    when E4 > E1 ->
+    depth_measurement_increases_from_sliding_windows([E2, E3, E4 | Rest],
+                                                     E2 + E3 + E4,
+                                                     Increases + 1);
+depth_measurement_increases_from_sliding_windows([_E1, E2, E3, E4 | Rest],
+                                                 _PreviousWindow,
+                                                 Increases) ->
+    depth_measurement_increases_from_sliding_windows([E2, E3, E4 | Rest],
+                                                     E2 + E3 + E4,
+                                                     Increases).
+
+%% HELPER FUNCTIONS
 convert_long_string_to_numbers(String) ->
     lists:map(fun(S) ->
                  {Value, _} = string:to_integer(S),
@@ -26,7 +57,6 @@ convert_long_string_to_numbers(String) ->
 convert_long_string_to_list(String) ->
     lists:filter(fun(Value) -> Value > "" end, string:split(String, "\n", all)).
 
-
 read_input_file_into_string(InputFileName) ->
     {ok, InputFromFile} = file:read_file(InputFileName),
     unicode:characters_to_list(InputFromFile).
@@ -34,5 +64,9 @@ read_input_file_into_string(InputFileName) ->
 main() ->
     InputFromDescription = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263",
     io:format("~p~n", [depth_measurement_increases(InputFromDescription)]),
+    io:format("~p~n",
+              [depth_measurement_increases_from_sliding_windows(InputFromDescription)]),
     InputFromFileAsString = read_input_file_into_string("input.txt"),
-    io:format("~p~n", [depth_measurement_increases(InputFromFileAsString)]).
+    io:format("~p~n", [depth_measurement_increases(InputFromFileAsString)]),
+    io:format("~p~n",
+              [depth_measurement_increases_from_sliding_windows(InputFromFileAsString)]).
