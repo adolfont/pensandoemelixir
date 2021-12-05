f49f89ea6d8dc580efe3202788ea433ea01a1947
Adolfo Neto <adolfo.usp@gmail.com>
2021-12-05 11:33:55 -0300

    Advent of Code - Day 5 - Erlang

--- adventofcode/2021/day05_2021/day05.erl
+++ adventofcode/2021/day05_2021/day05.erl
@@ -1,14 +1,18 @@
 -module(day05).
 
--export([main/0]).
+-export([main/0, all_pairs_2/1]).
 
 % Problem: https://adventofcode.com/2021/day/5
 
 % erlc day05.erl && erl -noshell -pa . -eval "day05:main()" -s init stop
 
 main() ->
+    io:fwrite("Part 1~n"),
     lazy_part_1("input_from_description.txt"),
-    lazy_part_1("input.txt").
+    lazy_part_1("input.txt"),
+    io:fwrite("Part 2~n"),
+    lazy_part_2("input_from_description.txt"),
+    lazy_part_2("input.txt").
 
 lazy_part_1(Filename) ->
     io:format("Filename: ~p~n", [Filename]),
@@ -20,11 +24,17 @@ lazy_part_1(Filename) ->
     % io:format("~p~n", [all_pairs({{5,9}, {0,9}})]).
     % io:format("All pairs generated from file:~n ~p~n",
     %       [generate_all_pairs(processed_input(Filename))]),
-    io:format("Resut: ~p~n", [length(generate_all_pairs(processed_input(Filename)))]).
+    io:format("Resut: ~p~n",
+              [length(generate_all_pairs(fun all_pairs/1, processed_input(Filename)))]).
 
-generate_all_pairs(List) ->
+lazy_part_2(Filename) ->
+    io:format("Filename: ~p~n", [Filename]),
+    io:format("Resut: ~p~n",
+              [length(generate_all_pairs(fun all_pairs_2/1, processed_input(Filename)))]).
+
+generate_all_pairs(F, List) ->
     L = lists:flatten(
-            lists:map(fun all_pairs/1, List)),
+            lists:map(F, List)),
     SL = lists:sort(L),
     USL = lists:usort(L),
     lists:usort(
@@ -40,6 +50,31 @@ all_pairs({{X1, Y}, {X2, Y}}) ->
     [{X, Y} || X <- lists:seq(X2, X1)];
 all_pairs(_) ->
     [].
+
+all_pairs_2({{X, Y1}, {X, Y2}}) when Y1 =< Y2 ->
+    [{X, Y} || Y <- lists:seq(Y1, Y2)];
+all_pairs_2({{X, Y1}, {X, Y2}}) ->
+    [{X, Y} || Y <- lists:seq(Y2, Y1)];
+all_pairs_2({{X1, Y}, {X2, Y}}) when X1 =< X2 ->
+    [{X, Y} || X <- lists:seq(X1, X2)];
+all_pairs_2({{X1, Y}, {X2, Y}}) ->
+    [{X, Y} || X <- lists:seq(X2, X1)];
+all_pairs_2({{X1, Y1}, {X2, Y2}}) when abs(X2 - X1) == abs(Y2 - Y1) ->
+    all_pairs_2_aux({{X1, Y1}, {X2, Y2}});
+all_pairs_2(_) ->
+    [].
+
+all_pairs_2_aux({{X1, Y1}, {X2, Y2}}) when X1 > X2 ->
+    all_pairs_2_aux({{X2, Y2}, {X1, Y1}});
+all_pairs_2_aux({{X1, Y1}, {X2, Y2}}) when Y1 < Y2 ->
+    [{X1 + Step, Y1 + Step} || Step <- range(0, X2 - X1)];
+all_pairs_2_aux({{X1, Y1}, {X2, _Y2}}) ->
+    [{X1 + Step, Y1 - Step} || Step <- range(0, X2 - X1)].
+
+range(X, Y) when X < Y ->
+    lists:seq(X, Y);
+range(X, Y) ->
+    lists:seq(Y, X).
 
 processed_input(InputFileName) ->
     parse(input(InputFileName)).
