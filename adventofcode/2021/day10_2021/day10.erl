48b5808c0b3da1cc8d203835f229f3614075d6bd
Adolfo Neto <adolfo.usp@gmail.com>
2021-12-10 08:16:23 -0300

    Day 10 of Advent of Code 2021 in Erlang

--- adventofcode/2021/day10_2021/day10.erl
+++ adventofcode/2021/day10_2021/day10.erl
@@ -9,7 +9,7 @@
 % erlc day10.erl && erl -noshell -pa . -eval "day10:main()" -s init stop
 %
 
-% learned: ???
+% learned: nothing new today
 
 -export([main/0]).
 
@@ -20,26 +20,53 @@
 main() ->
   main("input_from_description.txt"),
 
-   main("input.txt").
+  main("input.txt").
 
 main(File) ->
   Data = parse_file(file:read_file(File)),
 
-  io:format("Part 1 (26397, 364389) for ~p: ~p~n", [File, part1(Data)]).
+  io:format("Part 1 (26397, 364389) for ~p: ~p~n", [File, part1(Data)]),
 
-% io:format("Part 2 (1134, 1103130) for ~p: ~p~n", [File, part2(Data)]).
+  io:format("Part 2 (288957, 2870201088) for ~p: ~p~n", [File, part2(Data)]).
 
 part1(Data) ->
-  lists:sum([parse_line(Line) || Line <- Data]).
+  lists:sum([parse_line(Line) || Line <- Data, is_integer(parse_line(Line))]).
+
+part2(Data) ->
+  get_middle_score([score(parse_line(Line))
+                    || Line <- Data, not is_integer(parse_line(Line))]).
+
+get_middle_score(List) ->
+  lists:nth(1 + length(List) div 2, lists:sort(List)).
+
+% The score is determined by considering the completion string character-by-character.
+% Start with a total score of 0.
+% Then, for each character, multiply the total score by 5 and
+% then increase the total score by the point value given for the character
+% in the following table:
+% ): 1 point.
+% ]: 2 points.
+% }: 3 points.
+% >: 4 points.
+score(Line) ->
+  lists:foldl(fun(X, Acc) -> 5 * Acc + points(X) end, 0, Line).
+
+points($() ->
+  1;
+points($[) ->
+  2;
+points(${) ->
+  3;
+points($<) ->
+  4.
 
 parse_line(String) ->
   parse_line(String, []).
 
-
 parse_line([], OpenChars) when is_integer(OpenChars) ->
   OpenChars;
-parse_line([], _OpenChars)  ->
-  0;
+parse_line([], OpenChars) ->
+  OpenChars;
 parse_line([Head | Tail], OpenChars) ->
   parse_line_char(Head, Tail, OpenChars).
 
@@ -71,38 +98,6 @@ parse_line_char($}, _Tail, _) ->
   parse_line([], 1197);
 parse_line_char($>, _Tail, _) ->
   parse_line([], 25137).
-
-% parse_line(String) ->
-%   parse_line(String, 0,0,0,0).
-
-% parse_line([], Par, Bra, Cur, Bigger) -> [Par, Bra, Cur, Bigger];
-% parse_line([Head|Tail], Par, Bra, Cur, Bigger) ->
-%   parse_line_char(Head, Tail, Par, Bra, Cur, Bigger).
-
-% parse_line_char($(, Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par+1, Bra, Cur, Bigger);
-% parse_line_char($[, Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par, Bra+1, Cur, Bigger);
-% parse_line_char(${, Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par, Bra, Cur+1, Bigger);
-% parse_line_char($<, Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par, Bra, Cur, Bigger+1);
-% parse_line_char($), _Tail, Par, Bra, Cur, Bigger) when Par == 0 ->
-%   parse_line([], Par-1, Bra, Cur, Bigger);
-% parse_line_char($], _Tail, Par, Bra, Cur, Bigger) when Bra == 0 ->
-%   parse_line([], Par, Bra-1, Cur, Bigger);
-% parse_line_char($}, _Tail, Par, Bra, Cur, Bigger) when Cur == 0 ->
-%   parse_line([], Par, Bra, Cur-1, Bigger);
-% parse_line_char($>, _Tail, Par, Bra, Cur, Bigger) when Bigger == 0 ->
-%   parse_line([], Par, Bra, Cur, Bigger-1);
-% parse_line_char($), Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par-1, Bra, Cur, Bigger);
-% parse_line_char($], Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par, Bra-1, Cur, Bigger);
-% parse_line_char($}, Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par, Bra, Cur-1, Bigger);
-% parse_line_char($>, Tail, Par, Bra, Cur, Bigger) ->
-%   parse_line(Tail, Par, Bra, Cur, Bigger-1).
 
 parse_file({ok, RawData}) ->
   [unicode:characters_to_list(Line) || Line <- re:split(RawData, "[\n]+"), Line =/= <<>>];
