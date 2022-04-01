b8a51eb1e8c9c4bc733c52cf3130fcd31a33240f
Adolfo Neto <adolfo.usp@gmail.com>
2022-04-01 16:28:08 -0300

    Tarefa 1 do dia 2 de 2019 está pronta

--- adventofcode/2019/day02_2019/lib/day022019.ex
+++ adventofcode/2019/day02_2019/lib/day022019.ex
@@ -12,44 +12,49 @@ defmodule Day022019 do
   end
 
   def process_instruction(map, position) do
-    IO.puts("Instrução: #{map[position]}")
+    # IO.puts("Instrução: #{map[position]}")
 
     cond do
       map[position] == 99 ->
         map
 
       map[position] == 1 ->
-        add(map,position)
+        add(map, position)
         |> process_instruction(position + 4)
 
       map[position] == 2 ->
-        multiply(map,position)
+        multiply(map, position)
         |> process_instruction(position + 4)
-
     end
   end
 
   def add(map, position) do
-    position_input_1 = map[position+1]
-    position_input_2 = map[position+2]
-    position_output = map[position+3]
-    %{map | position_output => map[position_input_1]+map[position_input_2]}
+    position_input_1 = map[position + 1]
+    position_input_2 = map[position + 2]
+    position_output = map[position + 3]
+    %{map | position_output => map[position_input_1] + map[position_input_2]}
   end
 
   def multiply(map, position) do
-    position_input_1 = map[position+1]
-    position_input_2 = map[position+2]
-    position_output = map[position+3]
-    %{map | position_output => map[position_input_1]*map[position_input_2]}
+    position_input_1 = map[position + 1]
+    position_input_2 = map[position + 2]
+    position_output = map[position + 3]
+    %{map | position_output => map[position_input_1] * map[position_input_2]}
   end
+
   def process_file(filename) do
-    map = filename
+    filename
     |> convert_file_to_map()
     |> process_instruction(0)
+    |> Map.get(0)
+  end
 
-    IO.puts("Resultado: #{map[0]}")
+  def process_file_and_change(filename) do
+    filename
+    |> convert_file_to_map()
+    |> Map.put(1, 12)
+    |> Map.put(2, 2)
+    |> process_instruction(0)
+    |> Map.get(0)
   end
 end
-
-Day022019.process_file("input_from_description.txt")
-Day022019.process_file("input.txt")
--- adventofcode/2019/day02_2019/test/day022019_test.exs
+++ adventofcode/2019/day02_2019/test/day022019_test.exs
@@ -1,2 +1,13 @@
 defmodule Day022019Test do
+  use ExUnit.Case
+
+  test "Tests input from description" do
+    assert Day022019.process_file("input_from_description.txt") ==
+             3500
+  end
+
+  test "Tests my input" do
+    assert Day022019.process_file_and_change("input.txt") ==
+             5_290_681
+  end
 end
