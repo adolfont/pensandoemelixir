6e2ed37ed3d1ed92c7da876fdab725f74b737973
Adolfo Neto <adolfo.usp@gmail.com>
2022-12-05 09:41:16 -0300

    Day 5 AoC started

---
+++ adventofcode/2022/day05_2022/lib/day052022.ex
@@ -0,0 +1,18 @@
+defmodule Day052022 do
+  @moduledoc """
+  Documentation for `Day052022`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day052022.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ adventofcode/2022/day05_2022/test/day052022_test.exs
@@ -0,0 +1,8 @@
+defmodule Day052022Test do
+  use ExUnit.Case
+  doctest Day052022
+
+  test "greets the world" do
+    assert Day052022.hello() == :world
+  end
+end
