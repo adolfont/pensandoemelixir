d6f9e900ef548c06ab1875da906f9c7227bb6c93
Adolfo Neto <adolfo.usp@gmail.com>
2023-12-01 15:22:07 -0300

    aoc 2023 day1 part2

--- adventofcode/2023/day01/lib/day01.ex
+++ adventofcode/2023/day01/lib/day01.ex
@@ -1,6 +1,4 @@
 defmodule Day01 do
-  @digits ~w(one two three four five six seven eight nine)
-
   def task1(input) do
     input
     |> String.trim()
@@ -30,37 +28,61 @@ defmodule Day01 do
     input
     |> String.trim()
     |> String.split("\n")
-    |> IO.inspect()
     |> Enum.map(&task2_number/1)
     |> Enum.sum()
   end
 
-  defp task2_number(""), do: []
-  defp task2_number(string) do
-    parse_text_numbers =
-      for number <- @digits do
-        if String.starts_with?(string, number) do
-          {number, String.slice(string, String.length(number), String.length(string) - 1)}
-        end
-      end
-      |> Enum.filter(&(&1 != nil))
+  defp task2_number(str) do
+    list_with_numbers = task2_number(str, [])
 
-    IO.inspect(string)
-    case parse_text_numbers do
-      [{text_number, rest}] ->
-        [Enum.find_index(@digits, &(&1 == text_number)) + 1 | task2_number(rest)]
+    hd(list_with_numbers) * 10 + Enum.at(list_with_numbers, length(list_with_numbers) - 1)
+  end
 
-      [] ->
-        first = String.first(string)
-        rest = String.slice(string, 1, String.length(string) - 1)
+  defp task2_number("", current), do: Enum.reverse(current)
 
-        case Integer.parse(first) do
-          :error -> task2_number(rest)
-          {numeral, _} -> [numeral |  task2_number(rest)]
-        end
+  defp task2_number("one" <> rest, current) do
+    task2_number("ne" <> rest, [1 | current])
+  end
+
+  defp task2_number("two" <> rest, current) do
+    task2_number("wo" <> rest, [2 | current])
+  end
+
+  defp task2_number("three" <> rest, current) do
+    task2_number("hree" <> rest, [3 | current])
+  end
+
+  defp task2_number("four" <> rest, current) do
+    task2_number("our" <> rest, [4 | current])
+  end
+
+  defp task2_number("five" <> rest, current) do
+    task2_number("ive" <> rest, [5 | current])
+  end
+
+  defp task2_number("six" <> rest, current) do
+    task2_number("ix" <> rest, [6 | current])
+  end
+
+  defp task2_number("seven" <> rest, current) do
+    task2_number("even" <> rest, [7 | current])
+  end
+
+  defp task2_number("eight" <> rest, current) do
+    task2_number("ight" <> rest, [8 | current])
+  end
+
+  defp task2_number("nine" <> rest, current) do
+    task2_number("ine" <> rest, [9 | current])
+  end
+
+  defp task2_number(str, current) do
+    possible_value = String.first(str)
+    rest = String.slice(str, 1, String.length(str) - 1)
+
+    case Integer.parse(possible_value) do
+      :error -> task2_number(rest, current)
+      {numeral, _} -> task2_number(rest, [numeral | current])
     end
-    |> IO.inspect()
-
-    10
   end
 end
