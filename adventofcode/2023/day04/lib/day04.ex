f5619fb4e1b376fcf0efae6191c0cd4bb2d60f99
Adolfo Neto <adolfo.usp@gmail.com>
2023-12-04 11:42:48 -0300

    não está dando certo. Tento depois

--- adventofcode/2023/day04/lib/day04.ex
+++ adventofcode/2023/day04/lib/day04.ex
@@ -32,9 +32,10 @@ defmodule Day04 do
     input
     |> String.trim()
     |> String.split("\n")
-    |> IO.inspect()
     |> Enum.map(&get_points2/1)
-    |> Enum.sum()
+    |> process_result()
+
+    0
   end
 
   defp get_points2(input) do
@@ -47,14 +48,52 @@ defmodule Day04 do
     winning_numbers = MapSet.new(Enum.slice(list, 0..(bar_position - 1)))
     numbers = MapSet.new(Enum.slice(list, (bar_position + 1)..(length(list) - 1)))
 
-    size =
-      MapSet.intersection(winning_numbers, numbers)
-      |> MapSet.size()
+    MapSet.intersection(winning_numbers, numbers)
+    |> MapSet.size()
+  end
 
-    if size > 0 do
-      2 ** (size - 1)
-    else
-      0
+  defp process_result(list) do
+
+    all_ones = List.duplicate(1, length(list))
+
+    zipped = Enum.zip(
+      list,
+      all_ones
+    )
+
+    process_result_2(zipped, all_ones,1 )
+    |> IO.inspect()
+
+    0
+    # result = process_result(list, 0, List.duplicate(1, length(list)))
+  end
+
+  defp process_result_2([], result, _), do: result
+  defp process_result_2([head | tail] = list, result, n) do
+      IO.inspect({head, list, result})
+
+      {num, _} = head
+
+      process_result_2(tail, increment(result, n, num), n+1)
+  end
+
+
+  # defp process_result([], _, counter), do: counter
+
+  # defp process_result([head | tail]=list, position, counter) do
+  #   process_result(tail, position + 1, increment(counter, position+1, head))
+  # end
+
+  def increment(list, from_position, amount_of_values) do
+    IO.inspect {list, from_position, amount_of_values}
+    for i <- 0..length(list)-1 do
+      if (i >= from_position) and (i < from_position  +  amount_of_values) do
+        Enum.at(list, i)  + 1
+      else
+        Enum.at(list, i)
+      end
     end
+    |> IO.inspect()
+
   end
 end
