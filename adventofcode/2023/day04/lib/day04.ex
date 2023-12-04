16558a6c1c4408cb944d258a79c3607f50080e2e
Adolfo Neto <adolfo.usp@gmail.com>
2023-12-04 10:41:03 -0300

    fixed part 1

--- adventofcode/2023/day04/lib/day04.ex
+++ adventofcode/2023/day04/lib/day04.ex
@@ -3,13 +3,41 @@ defmodule Day04 do
     input
     |> String.trim()
     |> String.split("\n")
-    |> IO.inspect()
     |> Enum.map(&get_points/1)
-    |> IO.inspect()
     |> Enum.sum()
   end
 
   defp get_points(input) do
+    list =
+      input
+      |> String.split()
+      |> Enum.drop(2)
+
+    bar_position = Enum.find_index(list, fn x -> x == "|" end)
+    winning_numbers = MapSet.new(Enum.slice(list, 0..(bar_position - 1)))
+    numbers = MapSet.new(Enum.slice(list, (bar_position + 1)..(length(list) - 1)))
+
+    size =
+      MapSet.intersection(winning_numbers, numbers)
+      |> MapSet.size()
+
+    if size > 0 do
+      2 ** (size - 1)
+    else
+      0
+    end
+  end
+
+  def part2(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> IO.inspect()
+    |> Enum.map(&get_points2/1)
+    |> Enum.sum()
+  end
+
+  defp get_points2(input) do
     list =
       input
       |> String.split()
